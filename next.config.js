d7cba95c1f6d14b45bca97e151b5404405eba50f
유영민 <0min1320@kakao.com>
2025-04-14 09:07:33 +0900

    build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
-	experimental: { },
-	allowedDevOrigins: ['http://localhost:3000'],
+	experimental: { }
 }
 
 module.exports = nextConfig
