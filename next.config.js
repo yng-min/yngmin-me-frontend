9420a5b086b0ad8b1aaa52a5735743a0989a6d9f
유영민 <0min1320@kakao.com>
2025-04-19 05:43:37 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+        serverActions: true, // 서버 액션 사용 시
+    },
     webpack(config, { dev, isServer }) {
         if (!dev) {
-            config.cache = false; // 캐시 비활성화
+            // 프로덕션에서만 캐시 비활성화
+            config.cache = false;
         }
         return config;
     },
+    output: 'standalone',  // 서버리스 방식으로 동작
 };
+
+module.exports = nextConfig;
