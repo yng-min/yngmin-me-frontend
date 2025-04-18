8869226e09f93adac717f34ba31901cd8a9df58d
유영민 <0min1320@kakao.com>
2025-04-19 03:45:04 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-
 module.exports = {
     experimental: {},
-    webpack: (config) => {
-        config.resolve.extensions.push('.mjs');
+    webpack(config, { dev, isServer }) {
+        if (!dev && !isServer) {
+            // 캐시를 디스크 대신 메모리로 저장
+            config.cache = {
+                type: 'memory', // 메모리 캐시 사용
+            };
+        }
         return config;
     }
 };
