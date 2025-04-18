55eecf5c8b2ecd7c249e2d8937cdb77c06c263c4
유영민 <0min1320@kakao.com>
2025-04-19 05:46:40 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        serverActions: true, // 서버 액션 사용 시
-    },
-    webpack(config, { dev, isServer }) {
-        if (!dev) {
-            // 프로덕션에서만 캐시 비활성화
-            config.cache = false;
-        }
-        return config;
-    },
-    output: 'standalone',  // 서버리스 방식으로 동작
-};
+    output: 'standalone',
+    experimental: {},
+    headers: async () => {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0, must-revalidate'
+                    }
+                ]
+            }
+        ]
+    }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
